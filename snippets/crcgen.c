e2c6b023025f2dcd9a523377b90e5c60672741d2
Joe Allen <jhallenworld@gmail.com>
2014-10-19 20:04:00 -0400

    CRC stuff

---
+++ snippets/crcgen.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+unsigned short crcgen(unsigned int ch) 
+{
+	unsigned i, acc = 0;
+	ch <<= 8;
+	for (i = 8; i > 0; i--) {
+		if ((ch ^ acc) & 0x8000)
+			acc = (acc << 1) ^ 0x1021;
+		else
+			acc <<= 1;
+		ch <<= 1;
+        }
+	return acc;
+}
+
+int main(void) 
+{
+	unsigned int x, y;
+	for (y = 0; y < 32; y++) {
+	        printf("	dw	");
+		for (x = 0; x < 7; x++) {
+			printf("$%4.4X, ", crcgen(x + y * 8));
+                }
+		printf("$%4.4X\n", crcgen(x + y * 8));
+        }
+	return 0;
+}
