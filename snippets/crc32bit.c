7485d869bffd7618a15622ffa56c073eb060d167
Joe Allen <jallen@nklabs.com>
2017-04-04 11:51:06 -0400

    crc32 stuff

---
+++ snippets/crc32bit.c
@@ -0,0 +1,36 @@
+/* Calculate CRC32 a bit at a time */
+/* Big endian, MSB first version */
+
+#include <stdio.h>
+
+unsigned int crc;
+
+void docrc(int bit)
+{
+    int b = !!(crc & 0x80000000) ^ !!bit;
+    crc <<= 1;
+    if (b) {
+        crc ^= 0x04c11db7;
+    }
+}
+
+unsigned int crcb(unsigned char b)
+{
+	unsigned char m;
+	for (m = 0x80; m; m >>= 1)
+		docrc(b & m);
+	return crc;
+}
+
+int main(int argc, char *argv[])
+{
+	FILE *f = fopen(argv[1], "r");
+	if (f) {
+		int c;
+		while ((c = fgetc(f)) != -1) {
+			crcb(c);
+		}
+		fclose(f);
+		printf("CRC is %8.x\n", crc);
+	}
+}
---
+++ snippets/crcgen32.c
@@ -0,0 +1,37 @@
+/* Generate CRC32 table a bit at a time */
+/* Big endian, MSB first version */
+
+#include <stdio.h>
+
+unsigned int crc;
+
+void docrc(int bit)
+{
+    int b = !!(crc & 0x80000000) ^ !!bit;
+    crc <<= 1;
+    if (b) {
+        crc ^= 0x04c11db7;
+    }
+}
+
+unsigned int crcgen(unsigned char b)
+{
+	unsigned char m;
+	crc = 0;
+	for (m = 0x80; m; m >>= 1)
+		docrc(b & m);
+	return crc;
+}
+
+int main(void) 
+{
+	unsigned int x, y;
+	for (y = 0; y < 64; y++) {
+	        printf("	");
+		for (x = 0; x < 3; x++) {
+			printf("0x%8.8x, ", crcgen(x + y * 4));
+                }
+		printf("0x%8.8x\n", crcgen(x + y * 4));
+        }
+	return 0;
+}
